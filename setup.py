34f03988e0d4d880167f19ee2bb3d39ed6c607c6
gbromley <bromlgab@gmail.com>
2025-05-13 14:44:08 -0600

    setting up as package

--- setup.py
+++ setup.py
@@ -4,4 +4,16 @@ setup(
     name="odin",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pyserial",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "jupyter",
+            "matplotlib",
+        ],
+    },
+    python_requires=">=3.6",
 )
