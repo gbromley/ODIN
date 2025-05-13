96e132c49f49cfa481389de81dd4790deb0fe599
gbromley <bromlgab@gmail.com>
2025-05-13 14:40:24 -0600

    adding code to turn into package

---
+++ odin/__init__.py
@@ -0,0 +1,8 @@
+"""
+ODIN - Object Detection and Interception using Nerf
+
+A package for radar instrumentation and analysis, Kalman filtering,
+trajectory prediction, control systems, and robotics.
+"""
+
+__version__ = "0.1"
